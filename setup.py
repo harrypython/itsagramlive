7bd268f9e33b72ba3e4e9a47cbd2ef8152fcbae9
harrypython <harrypython@protonmail.com>
2020-04-03 11:50:34 -0700

    know we have a pip

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='ItsAGramLive',
-    version='0.1',
+    version='0.1.1',
     packages=setuptools.find_packages(),
     url='https://github.com/harrypython/itsagramlive',
     license='GPL-3.0',
