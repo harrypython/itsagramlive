176946603d423a39852b0d6e6ae25250710a4362
Angelo Ruggieri <angeloruggieridj@gmail.com>
2020-05-02 22:29:25 +0200

    Added the possibility to see the comments on a Live

--- ItsAGramLive/ItsAGramLive.py
+++ ItsAGramLive/ItsAGramLive.py
@@ -233,7 +233,7 @@ class ItsAGramLive:
                             users, ids = self.get_viewer_list()
                             print(users)
 
-                        elif cmd == 'get_comments':
+                        elif cmd == 'comments':
                             self.get_comments()
 
                         elif cmd[:4] == 'chat':
