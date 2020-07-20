caf080daee49a47f27f31b38afc49f8cf7025bc3
Raihan Stark <raihanyudosaputra30@gmail.com>
2020-07-20 16:10:19 +0700

    add custom cookiejar from string

---
+++ ItsAGramLive/http.py
@@ -0,0 +1,35 @@
+from io import BytesIO
+import sys
+import codecs
+import mimetypes
+import random
+import string
+
+import http.cookiejar as compat_cookiejar
+import pickle as compat_pickle
+
+class ClientCookieJar(compat_cookiejar.CookieJar):
+    """Custom CookieJar that can be pickled to/from strings
+    """
+    def __init__(self, cookie_string=None, policy=None):
+        compat_cookiejar.CookieJar.__init__(self, policy)
+        if cookie_string:
+            if isinstance(cookie_string, bytes):
+                self._cookies = compat_pickle.loads(cookie_string)
+            else:
+                self._cookies = compat_pickle.loads(cookie_string.encode('utf-8'))
+
+    @property
+    def auth_expires(self):
+        for cookie in self:
+            if cookie.name in ('ds_user_id', 'ds_user'):
+                return cookie.expires
+        return None
+
+    @property
+    def expires_earliest(self):
+        """For backward compatibility"""
+        return self.auth_expires
+
+    def dump(self):
+        return compat_pickle.dumps(self._cookies)
