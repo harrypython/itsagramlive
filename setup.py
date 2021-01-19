5b06f947c989c8a898d6f23520ef79c3c42bbe7e
harrypython <harrypython@protonmail.com>
2021-01-18 18:43:08 -0800

    PIP8 Fixes 'Bad password' error Fix #39

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name='ItsAGramLive',
-    version='1.4.3',
+    version='1.4.4',
     packages=setuptools.find_packages(),
     url='https://github.com/harrypython/itsagramlive',
     license='GPL-3.0',
