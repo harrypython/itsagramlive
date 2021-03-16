cff9397991d22eac296b6da4f7a7fc94d31f53a3
harrypython <harrypython@protonmail.com>
2021-03-16 12:03:55 -0700

    version 1.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name='ItsAGramLive',
-    version='1.5.5',
+    version='1.6',
     packages=setuptools.find_packages(),
     url='https://github.com/harrypython/itsagramlive',
     license='GPL-3.0',
